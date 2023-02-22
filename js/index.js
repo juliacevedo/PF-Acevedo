380af72cb841a7a1c0c573651834ccea85dbb4fd
Julia Acevedo <juliacevedo0103@gmail.com>
2023-02-22 18:53:13 -0300

    Fix menu mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-document.getElementById('menu_on').addEventListener('click', ()=>{
-    var elementos = document.querySelectorAll('body'); 
-    elementos.forEach(function(e) {
-       e.classList.toggle('visible_menu')
-})})
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('menu_on').addEventListener('click', ()=>{
+        var elementos = document.querySelectorAll('body'); 
+        elementos.forEach(function(e) {
+           e.classList.toggle('visible_menu')
+    })})
+});
+
