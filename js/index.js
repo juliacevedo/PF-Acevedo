876e04aef2683612ca5c64497690c06a7839d280
Julia Acevedo <juliacevedo0103@gmail.com>
2023-02-22 18:27:50 -0300

    Fix mobile menu

--- js/index.js
+++ js/index.js
@@ -1,28 +1,14 @@
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector("#menu_on");
-const cancelBtn = document.querySelector(".cancel-btn");
+const  button= document.getElementById('button-menu')
+const navclose = document.getElementById('nav')
 
+button.addEventListener('click',() => {
+  button.classList.toggle('close')
+  navclose.classList.toggle('visible_menu')
+})
 
-menuBtn.onclick = ()=>{
-  menu.classList.add("active");
-  menuBtn.classList.add("hide");
-  cancelBtn.classList.add("show");
-  body.classList.add("disabledScroll");
-}
-
-cancelBtn.onclick = ()=>{
-  menu.classList.remove("active");
-  menuBtn.classList.remove("hide");
-  cancelBtn.classList.remove("show");
-  body.classList.remove("disabledScroll");
-}
-
-window.onscroll = ()=>{
-  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
-
-$( document ).ready(function() {
-    $('#menu_on').click(function() {$('body').toggleClass('visible_menu');})
+navclose.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navclose.classList.remove('visible_menu')
+    toggleButton.classList.remove('close')
+  }
 })
