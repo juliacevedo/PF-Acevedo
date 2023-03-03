37b8b663288a9a0d850e88af6aed0841f96d1bc3
Julia Acevedo <juliacevedo0103@gmail.com>
2023-03-03 00:04:44 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -5,4 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
            e.classList.toggle('visible_menu')
     })})
 });
-
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('menu_on').addEventListener('click', ()=>{
+        var elementos = document.querySelectorAll('nav'); 
+        elementos.forEach(function(e) {
+            e.classList.toogle('color')  
+    })})
+});
